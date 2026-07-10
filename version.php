94bce509de11bb4f951497cb2f0ba3aac2629508
root <root@vultr.guest>
2026-07-10 20:27:56 +0000

    changing in CSS make it responsive

--- version.php
+++ version.php
@@ -5,7 +5,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_sentaldocupload';
-$plugin->version = 202607020148;
+$plugin->version = 202607020150;
 $plugin->requires  = 2022041900; // Moodle 4.0+.
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->release = 'v0.9.4';
